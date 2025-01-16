6f7b7fcba53defe24263ca761e1672045c4e74ec
Aditya Mandal <mandaladitya1614@gmail.com>
2025-01-16 20:58:36 +0530

    initial commit

---
+++ anchor/tests/voting.spec.ts
@@ -0,0 +1,45 @@
+import * as anchor from '@coral-xyz/anchor'
+import {Program} from '@coral-xyz/anchor'
+import {Keypair, PublicKey} from '@solana/web3.js'
+import {Voting} from '../target/types/voting'
+import {startAnchor} from "solana-bankrun";
+import { BankrunProvider } from 'anchor-bankrun';
+
+const IDL = require('../target/idl/voting.json');
+
+const votingAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF")
+
+describe('voting', () => {
+  // Configure the client to use the local cluster.
+  
+
+  it('Initialize Poll', async () => {
+    const context = await startAnchor("",[{name : "voting", programId: votingAddress}],[])
+    const provider = new BankrunProvider(context);
+
+    const votingProgram = new Program<Voting>(
+      IDL,
+      provider
+    );
+
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "What is your favorite type of peanut butter?",
+      new anchor.BN(0),
+      new anchor.BN(1821246480)
+
+    ).rpc();
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8)],
+      votingAddress,
+    )
+    
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log(poll);
+
+    expect(poll.pollId.toNumber()).toEqual(1);
+    expect(poll.description).toEqual("What is your favorite type of peanut butter?");
+  });
+})
